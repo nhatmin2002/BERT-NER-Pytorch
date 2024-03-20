3f163919b73defaa6fcfbe071137bf0dbee7db10
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-03-20 22:30:33 +0700

    Update run_ner_crf.py

--- run_ner_crf.py
+++ run_ner_crf.py
@@ -351,7 +351,7 @@ def load_and_cache_examples(args, task, tokenizer, data_type='train'):
                                                 cls_token_segment_id=2 if args.model_type in ["xlnet"] else 0,#0
                                                 sep_token=tokenizer.sep_token,
                                                 # pad on the left for xlnet
-                                                pad_token=tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0],
+                                                pad_token=0,#tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0],
                                                 pad_token_segment_id=4 if args.model_type in ['xlnet'] else 0,#0
                                                 )
         if args.local_rank in [-1, 0]:
